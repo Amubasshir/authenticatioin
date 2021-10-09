25b16c23ecb5f35984d0a9cb71a0cd354f1a2582
Amubasshir <themubasshir07@gmail.com>
2021-10-09 11:59:10 +0600

    hooks

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
-        <div>
+        <div className="header">
             <Link to = "/home">Home</Link>
-            <link to = "/register">Register</link>
-            <link to = "/login">Login</link>
+            <Link to = "/register">Register</Link>
+            <Link to="/login">Login</Link>
+            <button>Log out</button>
         </div>
     );
 };
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,33 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
+import initializeAuthentication from "../Firebase/Firebase.init";
+
+
+initializeAuthentication();
+const useFirebase = () =>
+{
+    const [user, setUser] = useState({});
+    const [error, setError] = useState(' ');
+    const auth = getAuth()
+    const googleProvider = new GoogleAuthProvider();
+
+    const signInUsingGoogle = () =>
+    {
+        signInWithPopup(auth, googleProvider)
+            .then(result =>
+            {
+                console.log(result.user);
+            setUser(result.user)
+            })
+            .catch(error =>
+            {
+                setError(error.message);
+        })
+    }
+    return {
+        user,
+        error,
+        signInUsingGoogle
+    }
+}
+export default useFirebase;
